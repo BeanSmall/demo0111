dd681f5b139302a00a2284379019459ae261cb1f
BeanSmall <34729004+BeanSmall@users.noreply.github.com>
2021-01-11 17:02:48 +0800

    更新Main类

--- src/com/bdqn/zhang/Main.java
+++ src/com/bdqn/zhang/Main.java
@@ -3,6 +3,7 @@ package com.bdqn.zhang;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2222222");
     }
 }
